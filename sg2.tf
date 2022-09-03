f3024c00789017febc80ff8a55f46680427564cd
Rakesh Reddy Palle <rakeshreddykw@gmail.com>
2022-09-03 18:31:52 +0530

    adding files

--- sg2.tf
+++ sg2.tf
@@ -1,5 +1,5 @@
-resource "aws_security_group" "bastion" {
-  name        = "bastion-security"
+resource "aws_security_group" "bastion-new" {
+  name        = "bastion-new"
   description = "allow admin with ssh"
   vpc_id      = "vpc-07437dab44fb23fce"
 
@@ -20,7 +20,7 @@ resource "aws_security_group" "bastion" {
   }
 
   tags = {
-    Name      = "bastion-security"
+    Name      = "bastion-new"
     terraform = "true"
   }
 }
@@ -28,8 +28,8 @@ resource "aws_security_group" "bastion" {
 
 
 
-resource "aws_security_group" "apache" {
-  name        = "apache-demo"
+resource "aws_security_group" "apache-new" {
+  name        = "apache-demo-new"
   description = "allow apache"
   vpc_id      = "vpc-07437dab44fb23fce"
 
@@ -60,7 +60,7 @@ resource "aws_security_group" "apache" {
   }
 
   tags = {
-    Name      = "apache-demo"
+    Name      = "apache-demo-new"
     terraform = "true"
   }
 }
